00b09270aa90594f931c8eaf18a48cde744951f4
BuildTools <unconfigured@null.spigotmc.org>
2017-11-05 18:20:57 +0100

    Statusmessage added

--- Tutorial 3/src/me/firstflames/BlockListener.java
+++ Tutorial 3/src/me/firstflames/BlockListener.java
@@ -1,8 +1,10 @@
 package me.firstflames;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 
 public class BlockListener implements Listener {
@@ -16,7 +18,17 @@ public class BlockListener implements Listener {
 
 		if (!player.hasPermission("playerAbilities.allowed")) {
 			e.setCancelled(true);
+			player.sendMessage(ChatColor.AQUA + "Du hast keine Berechtigung! Bitte kontaktiere den Serveradministrator, wenn du glaubst, dies ist ein Fehler.");
 		}
 	}
 
+	@EventHandler
+	public void onBlockBreake(BlockBreakEvent e) {
+		Player player = e.getPlayer();
+
+		if (!player.hasPermission("playerAbilities.allowed")) {
+			e.setCancelled(true);
+			player.sendMessage(ChatColor.AQUA + "Du hast keine Berechtigung! Bitte kontaktiere den Serveradministrator, wenn du glaubst, dies ist ein Fehler.");
+		}
+	}
 }
